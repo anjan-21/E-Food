94f557830c3b4e14253598439a016d965a9c711c
Anjan Koradia <anjankoradia2101@gmail.com>
2020-11-20 21:40:20 +0530

    Remove on click functionality & add on scroll and on click change navbar links background-color functionality.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 $(document).ready(function () {
-  $(".up a").click(function () {
-    $(window).scrollTop(0);
+  //   console.log($(window).scrollTop());
+
+  $(document).scroll(function () {
+    let scrollTop = $(window).scrollTop();
+    console.log($(window).scrollTop());
+
+    if (scrollTop >= 0 && scrollTop <= 929) {
+      $("#home").removeClass("on");
+      $("#step").removeClass("on");
+      $("#review").removeClass("on");
+      $("#about").removeClass("on");
+    }
+    if (scrollTop >= 930 && scrollTop <= 2000) {
+      $("#step").addClass("on");
+      $("#home").removeClass("on");
+      $("#review").removeClass("on");
+      $("#about").removeClass("on");
+    }
+    if (scrollTop >= 2100 && scrollTop <= 2300) {
+      $("#home").removeClass("on");
+      $("#step").removeClass("on");
+      $("#review").addClass("on");
+      $("#about").removeClass("on");
+    }
+    if (scrollTop >= 2350) {
+      $("#home").removeClass("on");
+      $("#step").removeClass("on");
+      $("#review").removeClass("on");
+      $("#about").addClass("on");
+    }
   });
 
-  $(document).on("click", ".navbar-nav li", function () {
-    $(this).addClass("on").siblings().removeClass("on");
+  $(".up a").click(function () {
+    $(window).scrollTop(0);
   });
 });
